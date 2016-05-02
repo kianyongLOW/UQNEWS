8f3cbe99fa0e64dec0798ed39929f7e4ee781706
kianyong <lowkianyong@Lows-MacBook.local>
2016-05-02 22:04:09 +1000

    updated the font awesome

--- php/all_require.php
+++ php/all_require.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
 <title>UQ NEWS</title>
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css"  href="css/style.css">
