5d17b1b9face53214c1c8031fa217b7e95d31237
vickyhaw94 <weikee070194@gmail.com>
2016-05-21 21:11:09 +1000

    pie done

--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,16 @@
+
 $.ajax({
 	type: "POST",
 	dataType: 'json',
 	url :"php/display-chart.php",
 	success: function (obj){
 		var sum = function(a, b) { return a + b };
+		var options = {
+  			width: "640px",
+  			height: "320px"
+		};
 
-	new Chartist.Pie('.ct-chart', obj, {
+	new Chartist.Pie('.ct-chart', obj, options, {
 	  labelInterpolationFnc: function(value) {
 		return Math.round(value / obj.series.reduce(sum) * 100) + '%';
 	  }
