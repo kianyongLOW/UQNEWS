337beef04c3a6dee4ee9cce112324bb8bc23c3f9
Chiam Cheng <chiamcheng@gmail.com>
2016-05-31 11:58:16 +1000

    UAX for mobile compatible

--- php/document-header.php
+++ php/document-header.php
@@ -2,6 +2,8 @@
 <html data-ng-app="uqnews" lang="en" ng-cloak>
 <head>
 <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>UQ NEWS</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
